fcf4ac7f39d9231dcc247928d5f077b66535b410
Muhammad Azeem <muhammad.azeem@arkhitech.com>
2016-03-22 10:46:35 +0500

    FizzBuzz code

--- FizzBuzz/ViewController.swift
+++ FizzBuzz/ViewController.swift
@@ -13,11 +13,53 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.fizzbuzz()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func fizzbuzz()
+    {
+        let result:NSMutableArray = NSMutableArray()
+        for (var i:Int = 1  ; i<101; i++)
+        {
+            if(i%3 == 0)
+            {
+                if(i%5 == 0)
+                {
+                    
+                    result.addObject("FizzBuzz")
+                }else
+                {
+                    result.addObject("Fizz")
+
+                }
+                
+            }
+            else if(i%5 == 0)
+            {
+                result.addObject("Buzz")
+
+            }
+            else if(i%3 != 0 && i%5 != 0)
+            {
+                result.addObject(i)
+
+            }
+        }
+        
+        for (var i:Int = 1  ; i<101; i++)
+        {
+            
+           let obj = result[i]
+            print(obj)
+        }
+        
     }
 
 
